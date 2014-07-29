318caa3d65b7065fcb4f61028c3cd663828a8af9
Andrew Thorp <andrewpthorp@gmail.com>
2014-07-28 23:13:58 -0500

    fix heroku

--- andrewthorp.js
+++ andrewthorp.js
@@ -1,14 +1,16 @@
 var path = require('path');
 var jade = require('jade');
+var logfmt = require("logfmt");
 var express = require('express');
 var app = express();
 
+app.use(logfmt.requestLogger());
 app.use('/css',express.static(path.join(__dirname, 'public/stylesheets')));
 app.get('/', function(req, res){
   var fn = jade.compileFile('./views/index.jade');
   res.send(fn());
 });
 
-var server = app.listen(3000, function() {
+var server = app.listen(process.env.PORT || 3000, function() {
     console.log('Listening on port %d', server.address().port);
 });
